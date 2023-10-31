d71f096655d1a10fca152f75451fdf06b2edd32c
katyyy111 <kasiakuzora@gmail.com>
2023-10-31 09:58:49 +0100

    Minor changeS

--- frontend/src/screens/TabBarScreen/TabBarScreen.js
+++ frontend/src/screens/TabBarScreen/TabBarScreen.js
@@ -6,13 +6,12 @@ import FriendsScreen from '../FriendsScreen';
 import MyTasksScreen from '../MyTasksScreen';
 import ProfileScreen from '../ProfileScreen';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Icon from 'react-native-vector-icons/Ionicons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import { NavigationContainer } from '@react-navigation/native';
 
 const Tab = createBottomTabNavigator();
 
 export default function TabBarScreen() {
-  
   return (
     <NavigationContainer independent={true}>
       <Tab.Navigator
@@ -21,15 +20,16 @@ export default function TabBarScreen() {
           headerShown: false,
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
-            if (route.name === 'Friends') {
-              iconName = focused ? 'home' : 'home-outline';
-            } else if (route.name === 'MyTasks') {
-              iconName = focused ? 'list' : 'list-outline';
-            } else if (route.name === 'Profile') {
-              iconName = focused ? 'settings' : 'settings-outline';
+            if (route.name == 'Friends') {
+              iconName = 'wifi';
+            } else if (route.name == 'MyTasks') {
+              iconName = 'trophy';
+            } else if (route.name == 'Profile') {
+              iconName = 'timelapse';
             }
+            console.log(iconName)
             // You can return any component that you like here!
-            return <Icon name={iconName} size={size} color={color} />;
+            return <Ionicons name={iconName} size={size} color={color} />;
           },
         })}
         options={{
